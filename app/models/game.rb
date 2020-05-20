02f4ca94d7f783c6b435ae336ac7f1c919b54409
mav-code <mavisansmembres@gmail.com>
2020-05-20 19:20:20 -0400

    made players optional, still have migration problems

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-    belongs_to :whiteplayer, :class_name => 'User'
-    belongs_to :blackplayer, :class_name => 'User'
+    belongs_to :whiteplayer, :class_name => 'User', optional: true
+    belongs_to :blackplayer, :class_name => 'User', optional: true
 
 end
