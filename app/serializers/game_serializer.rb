7b7931b8bc7720a318721347146e2d8830780d0d
mav-code <mavisansmembres@gmail.com>
2020-05-22 00:25:45 -0400

    added started

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,5 +1,5 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :fen, :pgn, :whiteplayer_id, :blackplayer_id, :name
+  attributes :id, :fen, :pgn, :whiteplayer_id, :blackplayer_id, :name, :started
   belongs_to :whiteplayer, :class_name => 'User'
   belongs_to :blackplayer, :class_name => 'User'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ User.destroy_all
 jim = User.create(username: "Jim", password: "123")
 bob = User.create(username: "Bob", password: "123")
 
-Game.create(name: 'the one game in our seedfile', whiteplayer_id: jim.id, blackplayer_id: bob.id, fen: 'none', pgn: 'none')
-Game.create(name: 'the two game in our seedfile', whiteplayer_id: bob.id, blackplayer_id: jim.id, fen: 'none', pgn: 'none')
+Game.create(name: 'the one game in our seedfile', whiteplayer_id: jim.id, blackplayer_id: bob.id, fen: 'none', pgn: 'none', started: false)
+Game.create(name: 'the two game in our seedfile', whiteplayer_id: bob.id, blackplayer_id: jim.id, fen: 'none', pgn: 'none', started: false)
 
 p "seed done"
